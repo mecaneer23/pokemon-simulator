43c13b4eeeb22ddde5506e79ffb7b30d2fe213b6
mecaneer23 <mecaneer23@gmail.com>
2024-04-06 19:29:49 -0500

    feat: add PokemonTrainer class

---
+++ PokemonTrainer.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class PokemonTrainer {
+    private static final int MAX_POKEMON = 2;
+
+    private String name;
+    private ArrayList<Pokemon> collection;
+
+    public PokemonTrainer(String name) {
+        this.name = name;
+        this.collection = new ArrayList<Pokemon>();
+    }
+
+    public boolean addPokemon(Pokemon pokemon) {
+        if (this.collection.size() > MAX_POKEMON) {
+            this.collection.add(pokemon);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean hasLost() {
+        for (Pokemon pokemon : this.collection) {
+            if (!pokemon.hasFainted()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Pokemon getNextPokemon() {
+        for (Pokemon pokemon : this.collection) {
+            if (!pokemon.hasFainted()) {
+                return pokemon;
+            }
+        }
+        return null;
+    }
+
+    public String toString() {
+        return this.name;
+    }
+}
