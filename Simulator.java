8027dcaded6909ded5a999df24672154a9674a95
mecaneer23 <mecaneer23@gmail.com>
2024-04-12 10:16:00 -0500

    feat: add main game loop and faint and win conditions

--- Simulator.java
+++ Simulator.java
@@ -16,11 +16,18 @@ public class Simulator {
         scan.nextLine();
         ComputerTrainer computer = new ComputerTrainer("Computer");
 
-        playerTurn(trainer, computer);
-        computerTurn(trainer, computer);
-        // add game loop
-        // check for pokemon fainting and print when they have
-        // add win condition
+        while (true) {
+            playerTurn(trainer, computer);
+            if (trainer.hasLost()) {
+                System.out.println(computer.getName() + " wins!");
+                break;
+            }
+            computerTurn(trainer, computer);
+            if (computer.hasLost()) {
+                System.out.println(trainer.getName() + " wins!");
+                break;
+            }
+        }
 
         scan.close();
     }
@@ -34,9 +41,13 @@ public class Simulator {
         }
         System.out.println(current);
         Move move = computer.chooseRandomMove();
+        Pokemon opponentPokemon = player.getNextPokemon();
         current.attack(player.getNextPokemon(), move);
         System.out.println(computer.getName() + "'s " + current.getName() + " used " + move + " on "
                 + player.getName() + "'s " + player.getNextPokemon());
+        if (opponentPokemon.hasFainted()) {
+            System.out.println(player.getName() + "'s " + opponentPokemon + " has fainted!");
+        }
         System.out.println("Press enter to continue...");
         scan.nextLine();
     }
@@ -55,9 +66,13 @@ public class Simulator {
         }
         System.out.print(player.getName() + ", choose your move: ");
         String moveName = scan.nextLine();
+        Pokemon opponentPokemon = computer.getNextPokemon();
         current.attack(computer.getNextPokemon(), moveName);
         System.out.println(player.getName() + "'s " + current.getName() + " used " + moveName + " on "
                 + computer.getName() + "'s " + computer.getNextPokemon());
+        if (opponentPokemon.hasFainted()) {
+            System.out.println(computer.getName() + "'s " + opponentPokemon.getName() + " has fainted!");
+        }
         System.out.println("Press enter to continue...");
         scan.nextLine();
     }
@@ -104,10 +119,3 @@ public class Simulator {
         }
     }
 }
-
-// A PokemonTrainer can only have one active Pokemon at a time.
-// When the active Pokemon faints, the PokemonTrainer will use their next
-// Pokemon.
-// When all of a PokemonTrainer’s Pokemon have fainted, the game is over.
-// Allow each PokemonTrainer to take turns choosing a Move for their current
-// Pokemon to use on the opponent Pokemon until one PokemonTrainer loses.
